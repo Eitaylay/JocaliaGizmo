38d5af0076e8add747f6a7952667f6cf49bbef04
eitaylay <eitaylay2011@gmail.com>
2021-08-23 06:26:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,148 @@
+from replit import clear
+import math
+from getkey import getkey
+from prettytable import PrettyTable
+
+
+going_back = False
+
+
+def gold_weight_converter(back):
+    """Take input of weight of 15 or 14 pal 2 pyar and change its weight to pure gold weight."""
+    while not back:
+        print("Gold weight converter")
+        print("Choose a method!\n 1. 15pal to 100%\n 2. 14pal 2pyar to 100% \n 0. back")
+        gwc_choice = getkey()
+        clear()
+        if gwc_choice == '0':
+            back = True
+
+        def gwc_method(gwc_choice, back):
+            # whereas s is မီးလင်း ,  b is ဒင်္ဂါးရွှေ
+
+            def a_to_s(s_price, weight):
+                gold_quality = "15 pal"
+                a_price = math.trunc(s_price * (16 / 17))
+                print(f"{gold_quality} gold price: {a_price}")
+                gwc_calculation(a_price, weight, gold_quality)
+
+            def b_to_s(s_price, weight):
+                gold_quality = "14 pal 2 pyar"
+                b_price = math.trunc(s_price * (16 / 17.5))
+                gwc_calculation(b_price, weight, gold_quality)
+            
+            def gwc_calculation(price, weight, quality):
+                x = PrettyTable()
+                yway_weight = float((kyat * 128 + pal * 8 + yway) / 128)
+                value = math.trunc(yway_weight * price)
+                s_pal, s_kyat = math.modf(value / s_price)
+                s_kyat = int(s_kyat)
+                s_yway, s_pal = math.modf(s_pal * 16)
+                s_pal = int(s_pal)
+                s_yway = round(s_yway * 8, 2)
+                if s_yway >= 8:
+                    s_pal += 1
+                    s_yway = s_yway - 8
+                if s_pal  >= 16:
+                    s_kyat += 1
+                    s_pal = s_pal - 16
+                s_weight = [s_kyat, s_pal, s_yway]
+                x.field_names = ["Quality", quality, "➟", "100%"]
+                x.add_row(["Price", price, " ", s_price])
+                x.add_row(["Weight", weight, " ", s_weight])
+                x.add_row(["Value", value, " ", " "])
+                print(x)
+
+
+            while not back:
+                s_price = int(input("Type pure gold price: "))
+                print("Type weight of the gold")
+                weight = []
+                weight.append(int(input("Kyat: ")))
+                weight.append(int(input("Pal: ")))
+                weight.append(float(input("Yway: ")))
+                kyat = weight[0]
+                pal = weight[1]
+                yway = weight[2]
+
+                if gwc_choice == '1':
+                    a_to_s(s_price, weight)
+                elif gwc_choice == '2':
+                    b_to_s(s_price, weight)
+                else:
+                    print("Invalid input")
+
+                print("\n'any keys' = again (or) '0' = return")
+                return_or_again = getkey()
+                clear()
+                if return_or_again == '0':
+                    back = True
+
+        gwc_method(gwc_choice, back)
+        
+
+def burmese_smith_length_converter(back):
+    """It is used to measure the bracelet's inside and outside of radius."""
+
+    while not back:
+        cm = float(input("Please type the length in centimeter!\n"))
+        pal, inch = math.modf(cm / 2.54)
+        inch = math.trunc(inch)
+        pal = round(pal * 16, 2)
+        print(f"{inch} inches & {pal} pal")
+        print("\n'any keys' = again (or) '0' = return")
+        return_or_again = getkey()
+        clear()
+        if return_or_again == '0':
+            back = True
+
+def profited_based_value(back):
+    """We use this program when we want to know the base value of a profited item."""
+
+    while not back:
+        value = int(input("What is the price?"))
+        percentage = int(input("what is the percentage?"))
+        act_value = 100 * value / (percentage + 100)
+        print(f"Main Value is {act_value}.")
+        print("\n'any keys' = again (or) '0' = return")
+        return_or_again = getkey()
+        clear()
+        if return_or_again == '0':
+            back = True
+
+
+def diamond_Ratisize_calculation(back):
+    """Traditional diamond size calculation"""
+
+    while not back:    
+        ct_weight = float(input("Please input the Carat Weight: "))
+        count_diamond= int(input("Please input the Diamond's count: "))
+        diamond_size = (11 * count_diamond) / (10 * ct_weight)
+        print(diamond_size)
+        print("\n'any keys' = again (or) '0' = return")
+        return_or_again = getkey()
+        clear()
+        if return_or_again == '0':
+            back = True
+
+
+while True:
+    print("Hi, I'm Jocalia Gizmo. What can I help you?")
+
+    print("Choose a service! \n 1. Gold weight converter \n 2. Burmese smith length converter \n 3. Profited based value \n 4. Diamond Rati-size calculation \n ")
+
+    main_input = getkey()
+    clear()
+
+    if main_input == '1':
+        gold_weight_converter(going_back)
+    elif main_input == '2':
+        burmese_smith_length_converter(going_back)
+    elif main_input == '3':
+        profited_based_value(going_back)
+    elif main_input == '4':
+        diamond_Ratisize_calculation(going_back)
+    else:
+        print("wrong input!")
+
+    
